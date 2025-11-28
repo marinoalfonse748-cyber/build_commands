d6d417e175a9b56ba2b36a22c17e3c0e584dcd9c
marinoalfonse748-cyber <marinoalfonse748@gmail.com>
2025-11-28 10:31:18 +0530

    Inital Build.sh

---
+++ build.sh
@@ -0,0 +1,61 @@
+#! /bin/bash
+
+rm -rf .repo/local_manifests
+repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.0 --git-lfs
+rm -rf prebuilts/clang/host/linux-x86
+
+echo "==> Syncing sources..."
+if [ -f /opt/crave/resync.sh ]; then
+    /opt/crave/resync.sh
+else
+    repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
+fi
+
+mf=(
+device/xiaomi/marble
+kernel/xiaomi/marble
+vendor/xiaomi/marble
+hardware/xiaomi
+kernel/xiaomi/marble-modules
+kernel/xiaomi/marble-devicetrees
+)
+
+rm -rf "${mf[@]}"
+
+
+echo "==> Cleaning old outputs and device/vendor/hardware trees..."
+dirs_to_remove=(
+    out/target/product/marble
+    out/target/product/gapps
+    out/target/product/vanilla
+    device/xiaomi/marble
+    kernel/xiaomi/marble
+    vendor/xiaomi/marble
+    hardware/xiaomi
+    kernel/xiaomi/marble-modules
+    kernel/xiaomi/marble-devicetrees
+)
+rm -rf "${dirs_to_remove[@]}"
+
+echo "=== Cloning device trees ==="
+git clone https://github.com/marinoalfonse748-cyber/device_xiaomi_marble device/xiaomi/marble
+git clone https://github.com/marinoalfonse748-cyber/vendor_xiaomi_marble vendor/xiaomi/marble
+git clone https://github.com/marinoalfonse748-cyber/android_kernel_xiaomi_marble kernel/xiaomi/marble
+git clone https://github.com/fiqri19102002/android_hardware_xiaomi hardware/xiaomi
+git clone https://github.com/marinoalfonse748-cyber/kernel_xiaomi_marble-devicetrees kernel/xiaomi/marble-devicestrees
+git clone https://github.com/marinoalfonse748-cyber/kernel_xiaomi_marble-modules kernel/xiaomi/marble-modules
+
+echo "=== Starting GMS (Pico) build ==="
+. build/envsetup.sh
+axion marble user gms pico
+ax -br
+mv out/target/product/marble out/target/product/gapps
+
+echo "=== Starting Vanilla (AOSP) build ==="
+. build/envsetup.sh
+axion marble user va
+ax -br
+mv out/target/product/marble out/target/product/vanilla
+
+
+echo "=== All builds completed successfully! ==="
